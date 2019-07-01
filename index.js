ccbccf324905a09a1179a0d4159820d632539d7c
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-01 20:43:08 +0200

    index.js clean up

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const express = require('express')
 const app = express();
-const mysql = require('mysql');
+const apiRouter = require('./server/routes')
+// const mysql = require('mysql');
 
 
-app.get('/', (req, res) => res.send('hello, world'));
+app.use(express.json());
 
-app.get('/api/films', (req, res) => {
-  res.send([1, 2, 3])
-})
+app.use('api/films', apiRouter)
 
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "user",
-  password: "password",
-  database: "starwarsapi"
-});
-
-con.connect(function (err) {
-  if (err) throw err;
-  con.query("SELECT * FROM films", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
-
-
-app.listen('3000', () => {
-  console.log('Server started on port 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`))

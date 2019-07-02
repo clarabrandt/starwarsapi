030ce13c4ea902226f73d78f3aaa23fe5d9b38d7
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-02 09:32:41 +0200

    minor fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express();
 const apiRouter = require('./server/routes')
-// const mysql = require('mysql');
 
 
 app.use(express.json());
 
-app.use('api/films', apiRouter)
+app.use('/api/films', apiRouter)
 
 
 const port = process.env.PORT || 3000;

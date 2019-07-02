9dbd6b4506feeb66f1a68723448f1503006f04b9
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-02 09:31:03 +0200

    changing createConnection to createPool on mysql

---
+++ server/db/people.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const express = require('express');
+const router = express.Router();
+
+const pool = mysql.createPool({
+  connectionLimit: 50,
+  host: "localhost",
+  user: "user",
+  password: "password",
+  database: "starwarsapi",
+  port: 3306
+});
+
+let filmsdb = {};
+
+
+
+
+
+module.exports = filmsdb;

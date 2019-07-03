3733e287eb06adc3f2a3e82223a29adfe76aa7cf
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-04 00:09:49 +0200

    including body-parser

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express();
 const apiRouter = require('./server/routes')
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.json());
 
-app.use('/api/films', apiRouter)
+app.use('/api', apiRouter)
+
+
 
 
 const port = process.env.PORT || 3000;

80c01c1cf2a70b91d11a5a8dd7cd5798de7c92d3
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-04 00:12:52 +0200

    change var filmsdb to db and add ORDER BY query selector

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const express = require('express');
-const router = express.Router();
+
 
 const pool = mysql.createPool({
   connectionLimit: 50,
@@ -11,15 +11,16 @@ const pool = mysql.createPool({
   port: 3306
 });
 
-let filmsdb = {};
+let db = {};
 
-filmsdb.search = (search_term) => {
+db.search = (search_term, order) => {
   return new Promise((resolve, reject) => {
     pool.query(`SELECT people.name, people.height
                   FROM people, films, people_in_film 
                   WHERE people_in_film.film = films.id 
                   AND people_in_film.people = people.id
-                  AND title LIKE '${search_term}'`,
+                  AND title LIKE '%${search_term}%'
+                  ORDER BY height ${order}`,
       (err, results) => {
         if (err) {
           return reject(err);
@@ -29,7 +30,4 @@ filmsdb.search = (search_term) => {
   })
 }
 
-
-
-
-module.exports = filmsdb;
+module.exports = db;

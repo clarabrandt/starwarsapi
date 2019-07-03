0836612ab46f9041abe173d2ab2fd9af290c7372
clarabrandt <mariaclarabrandt@gmail.com>
2019-07-03 11:00:01 +0200

    configuring the app to use body-parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express();
 const apiRouter = require('./server/routes')
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.use(express.json());
